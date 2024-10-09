9e3af74cc1a93ef459adf41f7c674f5dd71164b5
altafWartin <altaf@wartinlabs.com>
2024-10-09 18:22:50 +0530

    upadate ui

---
+++ src/Component/Footer/FooterMobile.js
@@ -0,0 +1,82 @@
+import React from 'react';
+
+const FooterMobile = () => {
+  return (
+<div className='main-container w-[calc(390*var(--rpx))] h-[calc(423*var(--rpx))] bg-[#fff] relative '>
+      {/* Sample image */}
+      <img
+        src='https://via.placeholder.com/100x18'
+        alt='Footer Image'
+        className='w-[calc(100*var(--rpx))] h-[calc(18*var(--rpx))] bg-cover bg-no-repeat relative z-[12] mt-[calc(26*var(--rpx))] mr-0 mb-0 ml-[calc(20*var(--rpx))]'
+      />
+      <span className="flex w-[calc(350*var(--rpx))] h-[calc(88*var(--rpx))] justify-start items-start font-['Nunito'] text-[calc(15*var(--rpx))] font-bold leading-[calc(22*var(--rpx))] text-[#cedfff] relative text-left z-[11] mt-[calc(20*var(--rpx))] mr-0 mb-0 ml-[calc(20*var(--rpx))]">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+      </span>
+      <div className='w-[calc(390*var(--rpx))] h-[calc(423*var(--rpx))] bg-[#004fe5] absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]' />
+      <div className='w-[calc(430*var(--rpx))] h-[calc(174*var(--rpx))] bg-[#065cff] rounded-[calc(10*var(--rpx))] border-solid border-t-[calc(1*var(--rpx))] border-t-[rgba(255,255,255,0.5)] border-solid border-b-[calc(1*var(--rpx))] border-b-[rgba(255,255,255,0.5)] absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-22.7%] z-[1]' />
+      <span className="flex h-[calc(18*var(--rpx))] justify-start items-start font-['Nunito'] text-[calc(15*var(--rpx))] font-bold leading-[calc(18*var(--rpx))] text-[#fff] absolute top-[calc(196*var(--rpx))] left-[calc(36*var(--rpx))] text-left whitespace-nowrap z-[3]">
+        Home
+      </span>
+      <span className="flex h-[calc(18*var(--rpx))] justify-start items-start font-['Nunito'] text-[calc(15*var(--rpx))] font-bold leading-[calc(18*var(--rpx))] text-[#fff] absolute top-[calc(196*var(--rpx))] left-[calc(152*var(--rpx))] text-left whitespace-nowrap z-[4]">
+        Recently Added
+      </span>
+      <span className="flex h-[calc(18*var(--rpx))] justify-start items-start font-['Nunito'] text-[calc(15*var(--rpx))] font-bold leading-[calc(18*var(--rpx))] text-[#fff] absolute top-[calc(232*var(--rpx))] left-[calc(36*var(--rpx))] text-left whitespace-nowrap z-[5]">
+        Catalog
+      </span>
+      <span className="flex h-[calc(18*var(--rpx))] justify-start items-start font-['Nunito'] text-[calc(15*var(--rpx))] font-bold leading-[calc(18*var(--rpx))] text-[#fff] absolute top-[calc(232*var(--rpx))] left-[calc(152*var(--rpx))] text-left whitespace-nowrap z-[6]">
+        Recently Trending
+      </span>
+      <span className="flex h-[calc(18*var(--rpx))] justify-start items-start font-['Nunito'] text-[calc(15*var(--rpx))] font-bold leading-[calc(18*var(--rpx))] text-[#fff] absolute top-[calc(268*var(--rpx))] left-[calc(36*var(--rpx))] text-left whitespace-nowrap z-[7]">
+        About Us
+      </span>
+      <span className="flex h-[calc(18*var(--rpx))] justify-start items-start font-['Nunito'] text-[calc(15*var(--rpx))] font-bold leading-[calc(18*var(--rpx))] text-[#fff] absolute top-[calc(268*var(--rpx))] left-[calc(152*var(--rpx))] text-left whitespace-nowrap z-[8]">
+        Contact Us
+      </span>
+      <span className="flex h-[calc(18*var(--rpx))] justify-start items-start font-['Nunito'] text-[calc(15*var(--rpx))] font-bold leading-[calc(18*var(--rpx))] text-[#fff] absolute top-[calc(304*var(--rpx))] left-[calc(36*var(--rpx))] text-left whitespace-nowrap z-[9]">
+        TOS
+      </span>
+      <span className="flex h-[calc(18*var(--rpx))] justify-start items-start font-['Nunito'] text-[calc(15*var(--rpx))] font-bold leading-[calc(18*var(--rpx))] text-[#fff] absolute top-[calc(304*var(--rpx))] left-[calc(152*var(--rpx))] text-left whitespace-nowrap z-10">
+        Privacy Policy
+      </span>
+      <div className='w-[calc(160*var(--rpx))] h-[calc(32*var(--rpx))] absolute top-[calc(366*var(--rpx))] left-1/2 translate-x-[9.38%] translate-y-0 z-[13]'>
+        <div className='w-[calc(32*var(--rpx))] h-[calc(32*var(--rpx))] absolute top-0 left-0 z-[14]'>
+          {/* Sample image */}
+          <img
+            src='https://via.placeholder.com/32x32'
+            alt='Footer Icon 1'
+            className='w-[calc(32*var(--rpx))] h-[calc(32*var(--rpx))]'
+          />
+        </div>
+        <div className='w-[calc(32*var(--rpx))] h-[calc(32*var(--rpx))] absolute top-[calc(0.000030517578125*var(--rpx))] left-[calc(85.333251953125*var(--rpx))] z-[19]'>
+          {/* Sample image */}
+          <img
+            src='https://via.placeholder.com/32x32'
+            alt='Footer Icon 2'
+            className='w-[calc(32*var(--rpx))] h-[calc(32*var(--rpx))]'
+          />
+        </div>
+        <div className='w-[calc(32*var(--rpx))] h-[calc(32*var(--rpx))] absolute top-[calc(0.000030517578125*var(--rpx))] left-[calc(42.666748046875*var(--rpx))] z-[24]'>
+          {/* Sample image */}
+          <img
+            src='https://via.placeholder.com/32x32'
+            alt='Footer Icon 3'
+            className='w-[calc(32*var(--rpx))] h-[calc(32*var(--rpx))]'
+          />
+        </div>
+        <div className='w-[calc(32*var(--rpx))] h-[calc(32*var(--rpx))] absolute top-[calc(0.000030517578125*var(--rpx))] left-[calc(128*var(--rpx))] z-[29]'>
+          {/* Sample image */}
+          <img
+            src='https://via.placeholder.com/32x32'
+            alt='Footer Icon 4'
+            className='w-[calc(32*var(--rpx))] h-[calc(32*var(--rpx))]'
+          />
+        </div>
+      </div>
+      <span className="flex h-[calc(18*var(--rpx))] justify-start items-start font-['Nunito'] text-[calc(14*var(--rpx))] font-bold leading-[calc(18*var(--rpx))] text-[#fff] absolute top-[calc(373*var(--rpx))] left-[calc(20*var(--rpx))] text-left whitespace-nowrap z-[2]">
+        © Crawlur, 2024
+      </span>
+    </div>
+  );
+};
+
+export default FooterMobile;
